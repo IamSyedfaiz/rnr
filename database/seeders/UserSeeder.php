121fbf9aa44f406b4d72950f83f25996390b45bb
Faiz miya <faizmiya110@gmail.com>
2024-04-18 11:20:40 +0530

    working

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'role' => 'admin',
+            'status' => 1,
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+    }
+}
